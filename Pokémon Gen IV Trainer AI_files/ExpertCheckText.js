ba9d571d0aaf35c45ce80c7df7f4afc077af4860
BPark16EK <haambitious2@gmail.com>
2025-10-30 03:47:09 -0400

    Update ExpertCheckText.js

--- Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
+++ Pokémon Gen IV Trainer AI_files/ExpertCheckText.js
@@ -355,11 +355,10 @@ If the target's HP is under 31%:
 	Score -1 and terminate`,
 
 ["InflictConfusionWendy"]:
-
-If the trainer is Cooltrainer Wendy, and this is the first turn of the battle:
+`If the trainer is Cooltrainer Wendy, and this is the first turn of the battle:
 	31% of a +2 scre and continue
 	
-`If the target's HP is over 70%:
+If the target's HP is over 70%:
 	No scoring change and terminate
 
 Unconditionally:
